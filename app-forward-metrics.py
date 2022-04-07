145e2a96fc5dd6b061644f4e4ce9176426296e3e
SaeedZahran <saeedzahran@hotmail.com>
2022-04-07 18:56:31 +0200

    Read the lead field matrix

--- app-forward-metrics.py
+++ app-forward-metrics.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 from pandas import np
 from scipy import linalg
 import scipy.io
+
+
+# Read the lead field matrix
+mat = scipy.io.loadmat('ga.mat')
+gain=mat["gaina"]
