7d283737c6864ca60890fc539bbdaaf97772c4bb
SaeedZahran <saeedzahran@hotmail.com>
2022-04-08 21:26:04 +0200

    sensitivity maps: starting from mne python example

--- app-forward-metrics.py
+++ app-forward-metrics.py
@@ -1,10 +1,24 @@
-# Import some libraries
+import numpy as np
+import mne
+from mne.datasets import sample
+from mne.source_space import compute_distance_to_sensors
+from mne.source_estimate import SourceEstimate
 import matplotlib.pyplot as plt
-from pandas import np
-from scipy import linalg
-import scipy.io
 
+print(__doc__)
 
-# Read the lead field matrix
-mat = scipy.io.loadmat('ga.mat')
-gain=mat["gaina"]
+data_path = sample.data_path()
+meg_path = data_path / 'MEG' / 'sample'
+fwd_fname = meg_path / 'sample_audvis-meg-eeg-oct-6-fwd.fif'
+subjects_dir = data_path / 'subjects'
+
+# Read the forward solutions with surface orientation
+fwd = mne.read_forward_solution(fwd_fname)
+mne.convert_forward_solution(fwd, surf_ori=True, copy=False)
+leadfield = fwd['sol']['data']
+print("Leadfield size : %d x %d" % leadfield.shape)
+
+grad_map = mne.sensitivity_map(fwd, ch_type='grad', mode='fixed')
+mag_map = mne.sensitivity_map(fwd, ch_type='mag', mode='fixed')
+eeg_map = mne.sensitivity_map(fwd, ch_type='eeg', mode='fixed')
+
